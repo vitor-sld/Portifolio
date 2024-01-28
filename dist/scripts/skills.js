4eb5a8b6ecde875a4f4e60199873b61768e28d9f
vitor-sld <vitor.cteles@gmail.com>
2024-01-28 20:04:55 -0300

    Adicionand textos corrigidos

--- dist/scripts/skills.js
+++ dist/scripts/skills.js
@@ -1,54 +1,49 @@
 comunicacao.onclick = () => {
     verificador(comunicacao);
-    resolucao.style.height = "75px";
-    trabalhoEquipe.style.height = "75px";
-    gestao.style.height = "75px";
-    lideranca.style.height = "75px";
+    resolucao.classList.remove("collapseOpen");
+    trabalhoEquipe.classList.remove("collapseOpen");
+    gestao.classList.remove("collapseOpen");
+    lideranca.classList.remove("collapseOpen");
 }
 
 resolucao.onclick = () => {
     verificador(resolucao);
-    comunicacao.style.height = "75px";
-    trabalhoEquipe.style.height = "75px";
-    gestao.style.height = "75px";
-    lideranca.style.height = "75px";
+    comunicacao.classList.remove("collapseOpen");
+    trabalhoEquipe.classList.remove("collapseOpen");
+    gestao.classList.remove("collapseOpen");
+    lideranca.classList.remove("collapseOpen");
 }
 
 trabalhoEquipe.onclick = () => {
     verificador(trabalhoEquipe);
-    comunicacao.style.height = "75px";
-    resolucao.style.height = "75px";
-    gestao.style.height = "75px";
-    lideranca.style.height = "75px";
+    comunicacao.classList.remove("collapseOpen");
+    resolucao.classList.remove("collapseOpen");
+    gestao.classList.remove("collapseOpen");
+    lideranca.classList.remove("collapseOpen");
 }
 
 gestao.onclick = () => {
     verificador(gestao);
-    comunicacao.style.height = "75px";
-    resolucao.style.height = "75px";
-    trabalhoEquipe.style.height = "75px";
-    lideranca.style.height = "75px";
+    comunicacao.classList.remove("collapseOpen");
+    resolucao.classList.remove("collapseOpen");
+    trabalhoEquipe.classList.remove("collapseOpen");
+    lideranca.classList.remove("collapseOpen");
 }
 
 lideranca.onclick = () => {
     verificador(lideranca);
-    comunicacao.style.height = "75px";
-    resolucao.style.height = "75px";
-    trabalhoEquipe.style.height = "75px";
-    gestao.style.height = "75px";
+    comunicacao.classList.remove("collapseOpen");
+    resolucao.classList.remove("collapseOpen");
+    trabalhoEquipe.classList.remove("collapseOpen");
+    gestao.classList.remove("collapseOpen");
 }
 
 
 
-
-
-function verificador (varVerificar){
-    if (varVerificar.style.height == "75px"){
-        varVerificar.style.height = "250px";
-    }else{
-        varVerificar.style.height = "75px";
+function verificador(varVerificar) {
+    if (varVerificar.classList.contains("collapseOpen")) {
+        varVerificar.classList.remove("collapseOpen");
+    } else {
+        varVerificar.classList.add("collapseOpen");
     }
 }
-
-
-// fazer ClassLIST 
